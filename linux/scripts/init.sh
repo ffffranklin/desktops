1dd9d571c53643fa63ae86035a1ea9b7d14a99f2
Franklin Clark <ffffranklin@pobox.com>
2020-06-08 13:50:06 +0000

    Init vim backup directories

--- linux/scripts/init.sh
+++ linux/scripts/init.sh
@@ -14,7 +14,13 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 # Packages                                                                    # 
 ###############################################################################
 
+# Init bash
 source `$SCRIPT_PATH/../../lib/bash/bootstrap.sh`
+
+# Init vim backup file directories
+mkdir -p ~/.vim/backups
+mkdir -p ~/.vim/swaps
+mkdir -p ~/.vim/undo
 
 
 echo "Done. Note that some of these changes require a logout/restart to take effect."
