84ec90cebf8c5c791142f6d5b6885554ba9d4888
Franklin Clark <franklin@modeanalytics.com>
2020-06-08 06:25:52 -0700

    Add init script

---
+++ linux/scripts/init.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+###############################################################################
+# Packages                                                                    # 
+###############################################################################
+
+source ../../lib/bash/bootstrap.sh
+
+
+echo "Done. Note that some of these changes require a logout/restart to take effect."
+
+
